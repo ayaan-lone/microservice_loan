22d6a0f9f47b8ef9e06445f90170f58acd3eafbc
Himanshu Singh Dhapola <himanshu.dhapola@xebia.com>
2024-06-30 12:20:15 +0530

    Health Check Controller added and initial setup

--- src/main/java/com/microservice/loan/LoanApplication.java
+++ src/main/java/com/microservice/loan/LoanApplication.java
@@ -1,7 +1,9 @@
 package com.microservice.loan;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LoanApplication {
@@ -9,5 +11,9 @@ public class LoanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoanApplication.class, args);
 	}
-
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
